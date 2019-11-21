889c07060c530b34c6a0d96f7ac796fb210675fe
Albert Kragl <albert.kragl@mail.mcgill.ca>
2019-11-21 16:42:02 -0500

    Add CMake to automate build process (#13)
    * Add CMake file in root directory and other required files (#4)
    
    * Update CMake version, rename top-level src folder, and update gitignore
    
    * Remove CMake autogenerated files

---
+++ cmake-utils/io/fs/CMakeFiles/fs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fs.dir/NFile.cc.o"
+  "libfs.a"
+  "libfs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
